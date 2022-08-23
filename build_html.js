b0df5e906d61271b8fd1d32009746980c5811b95
Michael Gallaspy <mgallaspy@cloudera.com>
2022-08-23 13:17:53 -0700

    Add google analytics tag

--- build_html.js
+++ build_html.js
@@ -595,24 +595,14 @@ ${makeStyle()}
 ${makeJS()}
 
 <!-- Google Analytics -->
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-53030428-21"></script>
 <script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-  ga('create', 'UA-157475426-9', 'auto');
-  ga('send', 'pageview');
-
-  window.addEventListener('load', function() {
-    document.getElementById('report-pdf-download').addEventListener('click', function() {
-      ga('send', {
-        hitType: 'pageview',
-        page: '//FF16-Structural_Time_Series-Cloudera_Fast_Forward.pdf'
-      });
-    });
-  })
-
+  gtag('config', 'UA-53030428-21');
 </script>
 <!-- End Google Analytics -->
 
